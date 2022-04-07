67aa1b8ce8dcbe33dc78c6130a7bdc34bb1d6c36
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2022-04-07 15:52:47 +0530

    My commit 2

--- src/com/testpackage/example/Hellodream.java
+++ src/com/testpackage/example/Hellodream.java
@@ -2,6 +2,6 @@ package com.testpackage.example;
 
 public class Hellodream {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("My name is jackass and I am pushing my first code from IntelliJ terminal");
     }
 }
